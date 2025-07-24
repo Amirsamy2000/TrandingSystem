d3c29aca2dbf50609979b3fa53bdd9fc71ee1c34
ALameer Azmy <alameersamy99@outlook.com>
2025-07-24 15:42:04 +0300

    add New View To Dispaly videos

---
+++ TradingSystem/Controllers/VideosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrandingSystem.Controllers
+{
+    public class VideosController : Controller
+    {
+
+        // this View For Display All Videos For Course Use CourseId
+        public IActionResult Videos(int CourseId)
+        {
+            var culture = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+            //@ViewData["Master"] = @locaizer[""]
+
+            return View();
+        }
+    }
+}
