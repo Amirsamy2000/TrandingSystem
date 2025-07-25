16681a146d366e3610950800a329814cc2407390
GergesSalah2two <gergess663@gmail.com>
2025-07-25 21:33:11 +0300

    CategoriesRepository

---
+++ TrandingSystem.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrandingSystem.Domain.Entities;
+using TrandingSystem.Domain.Interfaces;
+using TrandingSystem.Infrastructure.Data;
+
+namespace TrandingSystem.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository<Category> 
+    {
+        private readonly db23617Context _context;
+
+        public CategoryRepository(db23617Context context)
+        {
+            _context = context;
+        }
+        public Category Create(Category Object)
+        {
+            _context.Categories.Add(Object);
+            _context.SaveChanges();
+            return Object;
+        }
+
+        public List<Category> Read()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Course ReadById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course Update(Course Element)
+        {
+            throw new NotImplementedException();
+        }
+        public Course Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
