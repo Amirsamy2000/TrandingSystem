3c7039307d93aab9b5931734e76d028d620c64be
gerges salah <126210626+gergesSalah@users.noreply.github.com>
2025-07-28 18:30:49 +0300

    all is ok after merge

--- TradingSystem/Mapping/MappingProfile.cs
+++ TradingSystem/Mapping/MappingProfile.cs
@@ -15,6 +15,13 @@ namespace TrandingSystem.Mapping
             CreateMap<Course, AddCourseCommand>().ReverseMap();
             CreateMap<Course, CourseVM>().ReverseMap();
             CreateMap<Course, CourseDto>().ReverseMap();
+            // Add other mappings here as needed
+            // Map Dto ==> VideoDto to Video;
+            CreateMap<Video, VideoDto>()
+          .ForMember(dest => dest.Title, opt => opt.MapFrom((src, dest, _, context) =>
+              context.Items["culture"].ToString() == "ar" ? src.TitleAR : src.TitleEN))
+          .ForMember(dest => dest.Description, opt => opt.MapFrom((src, _, _, context) =>
+              context.Items["culture"].ToString() == "ar" ? src.DescriptionAR : src.DescriptionEN));
 
 
         }
