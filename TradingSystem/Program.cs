41447464b98d574dea2471499c9f0f7b12da0546
ALameer Azmy <alameersamy99@outlook.com>
2025-08-03 20:42:38 +0300

    00

--- TradingSystem/Program.cs
+++ TradingSystem/Program.cs
@@ -1,26 +1,18 @@
-using AutoMapper;
-using MediatR;
 using FluentValidation;
-using MediatR;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using NuGet.Protocol.Core.Types;
 using System.Globalization;
-using TrandingSystem.Domain.Entities;
-using TrandingSystem.Domain.Entities;
-using TrandingSystem.Domain.Interfaces;
 using TrandingSystem.Application.Dtos;
 using TrandingSystem.Application.Validators;
+using TrandingSystem.Domain.Entities;
 using TrandingSystem.Domain.Interfaces;
 using TrandingSystem.Infrastructure.Data;
 using TrandingSystem.Infrastructure.Repositories;
-
-using TrandingSystem.Domain.Interfaces;
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");;
 using TrandingSystem.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // ربط قاعدة البيانات
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -30,35 +22,22 @@ builder.Services.AddDbContext<db23617Context>(options =>
 
 // تسجيل AutoMapper و MediatR والخدمات الأخرى
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddMediatR(cfg => 
-    cfg.RegisterServicesFromAssembly(typeof(AddCourseCommand).Assembly));
-
-//builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-//builder.Services.AddScoped<IUserRepository, UserRepository>();
-//builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
-
-//builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<Role>()
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(TrandingSystem.Application.Features.Courses.Commands.AddCourseCommand).Assembly));
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IImageService, ImageService>();
-
+//builder.Services.AddScoped<IWasabiUploader, WasabiUploader>();
 // Identity مع Roles و EF Store
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<Role>()
     .AddEntityFrameworkStores<db23617Context>();
 
-// Add UnitOfWork
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Localization - ملف الموارد داخل Resources/
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-
+// Controllers + Localization + FluentValidation (مسجل Validator يدوياً)
 builder.Services.AddControllersWithViews()
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
     .AddDataAnnotationsLocalization();
@@ -77,8 +56,8 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
     options.SupportedUICultures = supportedCultures;
 });
 
-    //// هنا بنحدد الطريقة اللي المستخدم يقدر يحدد بيها اللغة (كوكي أو كويري أو هيدر)
-    //options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+var app = builder.Build();
+
 // Configure Middleware
 if (!app.Environment.IsDevelopment())
 {
