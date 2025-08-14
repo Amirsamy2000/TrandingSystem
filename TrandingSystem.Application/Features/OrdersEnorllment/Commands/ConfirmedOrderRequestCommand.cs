45ccc003c1190230d375c8851fa3aa249dadc541
ALameer Azmy <alameersamy99@outlook.com>
2025-08-14 21:08:16 +0300

    work in landing page

--- TrandingSystem.Application/Features/OrdersEnorllment/Commands/ConfirmedOrderRequestCommand.cs
+++ TrandingSystem.Application/Features/OrdersEnorllment/Commands/ConfirmedOrderRequestCommand.cs
@@ -12,15 +12,17 @@ namespace TrandingSystem.Application.Features.OrdersEnorllment.Commands
     public class ConfirmedOrderRequestCommand : IRequest<Response<bool>>
     {
         public int OrderId { get; set; }
-        //public int Status { get; set; }
+        public bool Status { get; set; }
         public DateTime CreatedAt { get; set; }
         public int  ConfirmedBy{set;get;}
 
-        public ConfirmedOrderRequestCommand(int orderid, DateTime createdAt, int confirmedBy)
+
+        public ConfirmedOrderRequestCommand(int orderid, DateTime createdAt, int confirmedBy, bool status)
         {
             OrderId = orderid;
             CreatedAt = createdAt;
             ConfirmedBy = confirmedBy;
+            Status = status;
         }
 
     }
