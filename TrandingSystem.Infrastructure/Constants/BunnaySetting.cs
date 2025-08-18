ecef3b27a5171df76db6f301feacfbbc173f9908
ALameer Azmy <alameersamy99@outlook.com>
2025-08-18 21:17:42 +0300

    change color layout

--- TrandingSystem.Infrastructure/Constants/BunnaySetting.cs
+++ TrandingSystem.Infrastructure/Constants/BunnaySetting.cs
@@ -8,9 +8,11 @@ namespace TrandingSystem.Infrastructure.Constants
 {
     public static class BunnaySetting
     {
-        public   const string libraryId = "475986";
-        public  const string apiKey = "634e056f-4d4b-4e1b-8b48e0c99b52-f962-424c";
-        public const string signingKey = "4c3442df-623e-4910-a8fd-61aaf93aa812";
+        public   const string libraryId = "482059";
+        //public  const string apiKey = "634e056f-4d4b-4e1b-8b48e0c99b52-f962-424c";
+        public  const string apiKey = "4582e8fb-6dd3-4a89-b336ef140e16-7fe7-49a4";
+        //public const string signingKey = "4c3442df-623e-4910-a8fd-61aaf93aa812";
+        public const string signingKey = "aeee54b2-fd1c-4a04-bd92-9a312de95e11";
   
     }
 }
